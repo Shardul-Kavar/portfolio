b78d79b239a87a11d9ef297fa55811867fb2f042
Shardul <shardulpatel010@gmail.com>
2025-07-29 17:07:07 +0530

    Add Chess link to ContactsCard and update constants

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,21 +4,18 @@ import netlify from "@astrojs/netlify";
 import robotsTxt from "astro-robots-txt";
 import UnoCSS from "@unocss/astro";
 import icon from "astro-icon";
-
 import solidJs from "@astrojs/solid-js";
 import { remarkReadingTime } from "./src/lib/remark-reading-time.mjs";
-
 import svelte from "@astrojs/svelte";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://gianmarcocavallo.com/",
+  site: "https://radiant-bublanina-b52726.netlify.app/",
   integrations: [
     sitemap(),
     robotsTxt({
       sitemap: [
-        "https://gianmarcocavallo.com/sitemap-index.xml",
-        "https://gianmarcocavallo.com/sitemap-0.xml",
+        "https://radiant-bublanina-b52726.netlify.app/sitemap-index.xml",
+        "https://radiant-bublanina-b52726.netlify.app/sitemap-0.xml",
       ],
     }),
     solidJs(),
@@ -29,8 +26,8 @@ export default defineConfig({
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
-  output: "server",
-  adapter: netlify({ edgeMiddleware: true }),
+  output: "static",
+  adapter: netlify(),
   vite: {
     assetsInclude: "**/*.riv",
   },
--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -3,6 +3,7 @@ export const LINKS = {
   linkedin: "https://in.linkedin.com/in/shardul-patel-65a877114",
   discord: "https://discordapp.com/users/1399286980960980992",
   email: "shardulpatel010@gmail.com<",
+  chess: "https://www.chess.com/member/addictedsk"
 };
 
 export const loaderAnimation = [
