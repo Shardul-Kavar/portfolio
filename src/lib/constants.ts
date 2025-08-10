d8b36fd6dccf53c04d89e9b3060c060b75606a69
Shardul <shardulpatel010@gmail.com>
2025-08-10 18:17:53 +0530

    fix: Update LinkedIn link in constants.ts to correct URL

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,6 +1,6 @@
 export const LINKS = {
   github: "https://github.com/Shardul-Kavar",
-  linkedin: "https://in.linkedin.com/in/shardul-patel-65a877114",
+  linkedin: "https://www.linkedin.com/in/shardul-kavar/",
   discord: "https://discordapp.com/users/1399286980960980992",
   email: "shardulpatel010@gmail.com<",
   chess: "https://www.chess.com/member/addictedsk"
