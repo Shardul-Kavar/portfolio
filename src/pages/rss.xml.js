62e882d9d4c389f62c6ddeb031d42a5f3a831b4b
Shardul <shardulpatel010@gmail.com>
2026-01-04 20:54:43 +0530

    Feat: Updated full stack profile

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -7,7 +7,7 @@ const parser = new MarkdownIt();
 export async function GET(context) {
   const blog = await getCollection("blog");
   return rss({
-    title: "Shardul Patel's Blog",
+    title: "Shardul Kavar's Blog",
     description: "my blog",
     site: context.site,
     items: blog.map((post) => ({
